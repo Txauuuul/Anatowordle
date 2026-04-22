8914655bf5402c257ccba25dbaebd29a61e5e84d
Txauuuul <167421103+Txauuuul@users.noreply.github.com>
2026-04-22 13:05:58 +0200

    Versión final 22/04

--- auth/process_guest.php
+++ auth/process_guest.php
@@ -1,8 +1,7 @@
 <?php
 include '../includes/db.php';
 
-// Iniciamos una sesión marcada como invitado
-$_SESSION['usuario_id'] = null; // No tiene ID de base de datos
+$_SESSION['usuario_id'] = null;
 $_SESSION['username'] = "Invitado";
 $_SESSION['is_guest'] = true;
 
